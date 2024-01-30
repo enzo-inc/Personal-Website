dc2c44ccb64b9c4042e62dcee1053c1a2e47d7a2
Vincenzo Incutti <vincenzo.incutti@gmail.com>
2024-01-30 13:08:03 +0000

    fixed typos

--- app/content/bio.tsx
+++ app/content/bio.tsx
@@ -4,9 +4,9 @@ export const bioText = `
 
   I completed my BSc from the University of Edinburgh in Artificial Intelligence and Software Engineering in 2022, followed by a MSc in Financial Technology from the UCL Centre for Blockchain Technologies in 2023.
   
-  Since then, I have taken part in the Entrepreneur First graduate cohort in London, where I focused on finding a cofounder and bringing a start-up to life in an high-agency environment. While the journey at Entrepreneur First did not end in a successful venture, I am still working in the entreprenurship space.
+  Since then, I have taken part in the Entrepreneur First graduate cohort in London, where I focused on finding a cofounder and bringing a start-up to life in an high-agency environment. While the journey at Entrepreneur First did not end in a successful venture, I am still working in the entrepreneurship space.
 
   So if you are interested in building world-changing products and are looking for a strong technical cofounder, feel free to reach out.
 
-  I am currently interested in exploring high-social-impact applications of LLMs, specifically in the financial field. I believe that in the next five years the way both individuals and business are going to interact and think about money is going to radically change. This will be enabled by cheap LLMs running on-device and the rise of a new, more fluid digital economy enabled by blockcahin networks. I want to build that future. 
+  I am currently interested in exploring high-social-impact applications of LLMs, specifically in the financial field. I believe that in the next five years the way both individuals and business are going to interact and think about money is going to radically change. This will be enabled by cheap LLMs running on-device and the rise of a new, more fluid digital economy enabled by blockchain networks. I want to build that future. 
 `;
